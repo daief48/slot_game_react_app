1ff492352fcbbce4e75ace7f74e3b3184ce9f07e
daief48 <daief20103048@gmail.com>
2023-05-03 01:36:33 +0600

    app complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ function App() {
   return (
     <>
       <h1 className="heading_style">
-        Welcome to {" "}
-        <span style={{ fontWeight: "bold" }}>Slot machine games</span>
-      </h1>
-      <div className="slotmachine">
-        <SlotM x='😆' y='😆' z='😆' />
-        <SlotM x='😘' y='😆' z='😆'/> 
-        <SlotM x='🎊' y='🎊' z='😘'/>
+        🎰 Welcome to {" "}
+        <span style={{
+          fontWeight: "bold", background: '#c7c7c7',
+          padding: '3px 6px'
+        }}>Slot machine games</span>
+        🎰</h1>
+      <div className="slots">
+        <div className="slotmachine">
+          <SlotM x='😆' y='😆' z='😆' />
+          <SlotM x='😘' y='😆' z='😆' />
+          <SlotM x='🎊' y='🎊' z='😘' />
+        </div>
       </div>
     </>
   );
